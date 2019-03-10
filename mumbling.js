d8fac9a7eb7c1b663ca7716293638422a8d7f08c
Micahella Estacio <45410954+estmicahella@users.noreply.github.com>
2019-03-10 15:25:01 -0400

    Create mumbling.js

---
+++ mumbling.js
@@ -0,0 +1,12 @@
+function accum(s) {
+  return s 
+    .split('')
+    .map((char, index) => {
+      const charRepeated = char.repeat(index + 1)
+      return (
+      charRepeated.charAt(0).toUpperCase() + 
+      charRepeated.slice(1).toLowerCase()
+      )
+      })
+      .join('-')
+}
